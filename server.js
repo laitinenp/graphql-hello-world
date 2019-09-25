c60567809265584e65aba6b6e52654c2c8305750
Laitinen Petri <LaitinenP@karelia.fi>
2019-09-25 15:12:53 +0300

    server.js lisätty

---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql')
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  input SensorInput {
+	id: String!
+	value: Float
+	unit: String
+	documentation: String
+  }
+
+  type Sensor {
+    _id: ID!
+	id: String!
+    value: Float
+    unit: String
+	documentation: String
+  }
+
+  type Query {
+    getSensor(id: String): Sensor
+  }
+
+  type Mutation {
+    createSensor(input: SensorInput): Sensor
+    updateSensor(id: String, input: SensorInput): Sensor
+  }
+`);
+
+// If Message had any complex fields, we'd put them on this object.
+class Sensor {
+  constructor(_id, {id, value, unit, documentation}) {
+    this._id = _id
+	this.id = id
+    this.value = value
+    this.unit = unit
+	this.documentation = documentation
+  }
+}
+
+// Maps username to content
+var fakeDatabase = {}
+
+var root = {
+  getSensor: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no sensor exists with id ' + id);
+    }
+    return new Sensor(id, fakeDatabase[id])
+  },
+  createSensor: function ({input}) {
+    // Create a random id for our "database".
+    var id = input.id
+
+    fakeDatabase[id] = input
+    return new Sensor(id, input)
+  },
+  updateSensor: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no sensor exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input
+    return new Sensor(id, input)
+  },
+};
+
+var app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
